080a7285e524f806963e14445dac253c0f36bf8c
alliedesign <allie.pakele@gmail.com>
2026-07-01 23:56:30 -0700

    Implement hidden subscription form for Netlify
    Add hidden form for Netlify subscriptions.

--- app/layout.js
+++ app/layout.js
@@ -92,6 +92,10 @@ export default function RootLayout({ children }) {
           </div>
         </div>
         {children}
+<form name="subscribers" data-netlify="true" netlify-honeypot="bot-field" hidden>
+  <input type="email" name="email" />
+  <input type="text" name="bot-field" />
+</form>
       </body>
     </html>
   );
